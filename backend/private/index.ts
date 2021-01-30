3365260261003d9a29a08ade92559c186eddf49d
wwalpha <wwalpha@gmail.com>
2021-01-30 12:43:47 +0000

    update

--- backend/private/index.ts
+++ backend/private/index.ts
@@ -8,11 +8,11 @@ app.get('/', (_, res) => res.status(200).send());
 // private endpoint
 app.get('/endpoint', async (_, res) => {
   try {
-    // const response = await axios.get('http://localhost:51678/v1/metadata');
+    const response = await axios.get(`${process.env.ECS_CONTAINER_METADATA_URI_V4}/task`);
 
-    // console.log(response.data);
+    console.log(response.data);
 
-    // res.send(response.data);
+    res.send(response.data);
     console.log(process.env.ECS_CONTAINER_METADATA_URI_V4);
     res.send('private task1');
   } catch (err) {
