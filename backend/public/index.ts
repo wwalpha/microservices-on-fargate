2d49cc26f369d3c300222f25816e13192674d1e1
wwalpha <wwalpha@gmail.com>
2021-01-30 00:25:30 +0000

    update public repo

--- backend/public/index.ts
+++ backend/public/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
 // health check
-app.get('/api', (_, res) => res.status(200).send());
+app.get('/', (_, res) => res.status(200).send());
 // public service
 app.get('/api/public', (_, res) => res.send('Hello world'));
 // private service
-app.get('/api/private', (_, res) => res.send('Hello world'));
+app.get('/api/private', async (_, res) => {
+  const response = await axios.get('http://xxxx/endpoint');
+
+  res.send(response.data);
+});
 
 app.listen(8080, () => console.log('started at port 8080'));
