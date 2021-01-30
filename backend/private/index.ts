0b304307db84fa1d0d0e03996159fae67a1a14a7
wwalpha <wwalpha@gmail.com>
2021-01-30 11:43:37 +0000

    add logs

--- backend/private/index.ts
+++ backend/private/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
 // health check
 app.get('/', (_, res) => res.status(200).send());
 // private endpoint
-app.get('/endpoint', (_, res) => res.send('private task'));
+app.get('/endpoint', async (_, res) => {
+  console.log(process.env.xx);
+
+  const response = await axios.get('http://localhost:51678/v1/metadata');
+
+  console.log(response.data);
+
+  res.send(response.data);
+});
 
 app.listen(8090, () => console.log('started at port 8090'));
