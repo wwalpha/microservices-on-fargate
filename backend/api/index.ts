ff5f30f611edcddf63bb18203df10d56b2b84d13
wwalpha <wwalpha@gmail.com>
2021-02-01 22:42:33 +0000

    bugfix

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -25,7 +25,7 @@ app.get('/api/auth', async (_, res) => {
 // backend worker service
 app.get('/api/worker', async (_, res) => {
   try {
-    const response = await axios.get('http://worker.backend.microservice.local:8080/endpoint');
+    const response = await axios.get('http://worker.backend.microservice.local:8090/endpoint');
 
     res.send(response.data);
   } catch (err) {
